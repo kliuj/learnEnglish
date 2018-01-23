793d46c322271e2ed5f3754daccdae9d755853d1
kerwinliu <kerwinliu@qiyi.com>
2018-01-23 16:20:47 +0800

    update

--- src/pages/about/about.vue
+++ src/pages/about/about.vue
@@ -12,10 +12,7 @@
         <!-- MAINVIEW -->
         <div class="mainview">
             <section class="about-ledge">
-                <div><img src="../../../Assets/Images/about_01.jpg" width="100%"></div>
-                <p>Ledge为前沿国际会议提供专业语言服务和同声翻译解决方案，致力于打造中高端口译人才和行业领域专业人才学习、交流及跨界合作的平台，我们的核心理念是：社群学习、深挖行业、实战为王。</p>
-                <p>Ledge provides Simultaneous Interpreting and other language solutions for leading edge international conferences. Combining community learning，industry research with onsite case study，together we are building a training and collaboration platform for professional interpreters and talents from all industries to share and grow.</p>
-                <p>Copyright &copy; 2018 Ledge.cn, Inc. All rights reserved.</p>
+                <div><img src="../../../Assets/Images/pic_hd.jpg" width="100%"></div>
             </section>
         </div>
         <!-- //MAINVIEW -->
